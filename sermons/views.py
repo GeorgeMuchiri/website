6cf344b0899205eb9eb50f7cbdb32c4cfd7d3766
George Muchiri <gmuchirih@gmail.com>
2019-08-01 00:01:36 +0300

    Dev of Sermons

--- sermons/views.py
+++ sermons/views.py
@@ -4,8 +4,16 @@ from sermons.models import Teacher, Sermon, Category
 
 
 def index(request):
-    p = Sermon.objects.all()
-    context = {'teacher': p}
+    marriage = Sermon.objects.filter(category__name="Pillars of Marriage").order_by("-date_created")[:1]
+    foundation = Sermon.objects.filter(category__name="Foundation Class").order_by("-date_created")[:1]
+    wisdom = Sermon.objects.filter(category__name="Pillars of Wisdom").order_by("-date_created")[:1]
+
+    context  = {
+    'marriage': marriage,
+    'foundation': foundation,
+    'wisdom': wisdom,
+     }
+
     return render(request, 'sermons/index.html', context)
 
 
@@ -13,9 +21,14 @@ def about(request):
     return render(request, 'sermons/about.html')
 
 def sermons(request):
-    posts = Sermon.objects.filter(category__name='Pillars of Marriage')
+
+    marriage = Sermon.objects.filter(category__name="Pillars of Marriage").order_by("-date_created")
+    foundation = Sermon.objects.filter(category__name="Foundation Class").order_by("-date_created")
+    wisdom = Sermon.objects.filter(category__name="Pillars of Wisdom").order_by("-date_created")
     context  = {
-    'posts': posts
+    'marriage': marriage,
+    'foundation': foundation,
+    'wisdom': wisdom,
      }
     return render(request, 'sermons/sermons.html', context)
 
