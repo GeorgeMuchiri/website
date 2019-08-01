98cacbb3f8e2b6bb01e91f849cc09dd119cd5c3a
George Muchiri <gmuchirih@gmail.com>
2019-08-01 17:11:19 +0300

    Logic of files added

--- sermons/admin.py
+++ sermons/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Teacher, Category, Sermon
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name',)
+
 
 class SermonAdmin(admin.ModelAdmin):
     list_display = ('title', 'teacher', 'date_created', 'category')
-    prepopulated_fields = {'slug': ('title',)}
+
 
 admin.site.register(Teacher)
 admin.site.register(Sermon, SermonAdmin)
--- sermons/models.py
+++ sermons/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=250, unique=True)
+    #slug = models.SlugField(max_length=250, unique=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -23,9 +23,9 @@ class Sermon(models.Model):
     recently_added = models.DateTimeField(auto_now_add=True)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    audi = models.FileField(upload_to='audio/', null=True)
-    notes = models.FileField(upload_to='notes/', null=True)
-    slug = models.SlugField(max_length=250, unique=True)
+    audi = models.FileField(upload_to='audio/', default=None, null= True, blank=True)
+    notes = models.FileField(upload_to='notes/', default=None, null=True, blank=True)
+    #slug = models.SlugField(max_length=250, unique=True)
 
     def __str__(self):
         return self.title
